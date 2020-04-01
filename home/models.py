b39c76aa3896dd99d3f76259976f974f1bb208ec
yucheng888 <60374626+yucheng888@users.noreply.github.com>
2020-03-31 19:48:02 -0700

    Update models.py

--- home/models.py
+++ home/models.py
@@ -108,3 +108,11 @@ class Requests(models.Model):
 
     def __str__(self):
         return self.user.username
+
+  #it is for login history  
+class AllLogin(models.Model):
+    user = models.ForeignKey(User,related_name="Requests_User_id", on_delete=models.CASCADE)
+    date= models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return str(self.user) + ': ' + str(self.date)
